16d7eb4d2769b985d3dfc6e3d37689970caeee1d
roopesh <imroopeshkumar@gmail.com>
2019-03-29 15:32:12 +1100

    edited menustudentid it is now complete need to add comments

--- menu_StudentID.py
+++ menu_StudentID.py
@@ -1,89 +1,29 @@
-#Author: 
-#Date:
+# Author:
+# Date:
 
 
-#Please write your own program here
+# Please write your own program here
 import sys
 import os
 import nerdScore_StudentID as nerdscore
 
-menuOptions = {1: 'Fandom_Score', 2: 'Hobbies_Score', 3: 'NoOfSports', 4: 'CalculateNerdScore', 5: 'PrintRation', 6: 'name'}
+menuOptions = {1: 'name', 2: 'Fandom_Score', 3: 'Hobbies_Score', 4: 'NoOfSports', 5: 'CalculateNerdScore', 6: 'PrintRating'}
 menuInput = None;
+#fandomScore, hobbiesScore,sportssum = None;
+
 
 class Student:
     def __init__(self, name, Fandom_Score, Hobbies_Score, NoOfSports):
         self.name = name
         self.Fandom_Score = Fandom_Score
-        self.name = Hobbies_Score
-        self.name = NoOfSports
+        self.Hobbies_Score = Hobbies_Score
+        self.NoOfSports = NoOfSports
 
-def Fandom_Score():
-    print('validating fandom score')
-    fandomscorevalidated = False
-    while(not fandomscorevalidated):
-        try:
-            fandom_score = int(input('enter fandom score'))
-            if(fandom_score < 0):
-                raise TypeError
-            else:
-                fandomscorevalidated = True
-                return fandom_score
-        except:
-            print('please try again for fandom score')
-
-def Hobbies_Score():
-        hobbiesscorevalidated = False
-        print('validating Hobbies_Score as a multiple of 4')
-        while(not hobbiesscorevalidated):
-            try:
-                hobbies_score = int(input('enter hobbies score'))
-                if(hobbies_score < 0 or (hobbies_score % 4) > 0):
-                    raise TypeError
-                else:
-                    hobbiesscorevalidated = True
-                    return hobbies_score
-            except:
-                print('please try again for hobbies score')
-
-                
+student = Student(None, None, None, None)
+studentList = [];
 
 
-
-
-
-def NoOfSports():
-        NoOfSports_validation = False
-        print('validating Hobbies_Score as a multiple of 4')
-        while(not NoOfSports_validation):
-            try:
-                noofsports = int(input('enter no of sports score'))
-                if(noofsports < 0):
-                    raise TypeError
-                else:
-                    NoOfSports_validation = True
-                    return noofsports
-                    print(student.name)
-                    #print(student.name + student.NoOfSports + student.Hobbies_Score)
-            except:
-                print('please try again for no of sports')
-
-
-
-
-
-
-def CalculateNerdScore():
-    try:
-        nerdscore.calculateSkillEquation(FandomScore, HobbiesScore, SportsNum)
-    except:
-        print('error calculating')
-        return 0
-
-def PrintRating():
-    pass
-
-
-def name(student):
+def name():
     print('inside name')
     try:
         name = str(input('enter student name'))
@@ -91,55 +31,120 @@ def name(student):
 
     except:
         print('error')
+def Fandom_Score():
+    print('validating fandom score')
+    fandomscorevalidated = False
+    while (not fandomscorevalidated):
+        try:
+            fandom_score = int(input('enter fandom score'))
+            if (fandom_score < 0):
+                raise TypeError
+            else:
+                fandomscorevalidated = True
+                return fandom_score
+        except:
+            print('please try again for fandom score')
 
 
+def Hobbies_Score():
+    hobbiesscorevalidated = False
+    print('validating Hobbies_Score as a multiple of 4')
+    while (not hobbiesscorevalidated):
+        try:
+            hobbies_score = int(input('enter hobbies score'))
+            if (hobbies_score < 0 or (hobbies_score % 4) > 0):
+                raise TypeError
+            else:
+                hobbiesscorevalidated = True
+                return hobbies_score
+        except:
+            print('please try again for hobbies score')
 
 
+def NoOfSports():
+    NoOfSports_validation = False
+    print('validating Hobbies_Score as a multiple of 4')
+    while (not NoOfSports_validation):
+        try:
+            noofsports = int(input('enter no of sports score'))
+            if (noofsports < 0):
+                raise TypeError
+            else:
+                NoOfSports_validation = True
+                return noofsports
+                print(student.name)
+                # print(student.name + student.NoOfSports + student.Hobbies_Score)
+        except:
+            print('please try again for no of sports')
+def CalculateNerdScore():
+    skillScore = 0  # intialize the output list
 
-def menu_validation(student, menuOptions):
-    userip = int(input('enter menu option'))
-    if(userip > 6 or userip< 1):
-        raise TypeError
-    else:
-        
-        #if(userip == 1):
-            #fandom_score = Fandom_Score_validation()
-            print(menuOptions[userip])
-            if(menuOptions[userip]=='CalculateNerdScore'):
-                CalculateNerdScore(getattr(student, menuOptions[1]),getattr(student, menuOptions[2]),getattr(student, menuOptions[3]))
-            setattr(student, menuOptions[userip], globals()[menuOptions[userip]]())
-            print(getattr(student, menuOptions[userip]))
-        #elif(userip == 2):
-            #Hobbies_Score_validation()
-            #pass
-        #elif(userip == 3):
-            #NoOfSports_validation()
-           # pass
-        #elif(userip == 4):
-            #CalculateNerdScore()
-            #pass
-        #elif(userip == 5):
-            #PrintRating()
-            #pass
-        #else:
-            #GetName()
-            #pass
-        
-while(True):
-    student1 = Student(None, None, None, None)
-    for option in menuOptions:
-        print(menuOptions[option]+'\n')
+    # Please write your own program here
     try:
-        validatedmenuoption = menu_validation(student1, menuOptions)
+        x = student.Fandom_Score;
+        y = student.Hobbies_Score;
+        z = student.NoOfSports;
+
+        if( x == None or y == None or z == None):
+            raise ValueError;
+
+        skillScore = (((((y * y) * 42) / (z + 1))) ** (1 / 2))*x
+        student.Fandom_Score = None
+        student.Hobbies_Score = None
+        student.NoOfSports = None
+        print(skillScore)
+        studentList.append(skillScore)
     except:
+        print('error calculating result all values not entered')
+        skillScore = 0
+
+def nerdRating(studentscore):
+    rating = 'undefined'
+    if(studentscore < 1):
+        return 'NerdLite'
+    elif (studentscore<10 ):
+        return 'Nerdlinger'
+    elif (studentscore<100 ):
+        return 'Nerdl'
+    elif (studentscore < 500 ):
+        return 'Nerdington'
+    elif (studentscore <1000):
+        return 'Nerdrometa'
+    elif (studentscore < 2000):
+        return 'Nerd Supreme'
+
+
+
+def PrintRating():
+    for student in studentList:
+        print(str(studentList.index(student)));
+        print( str(nerdRating(student)))
+        print('student ' + str(studentList.index(student)) + ' nerdClass is: '+ str(nerdRating(student)))
+
+
+
+
+def menu_validation(menuOptions):
+    userip = int(input('enter menu option'))
+    if (userip > 6 or userip < 1):
+        raise IOError
+
+        print(menuOptions[userip])
+    if(userip > 4 ):
+        globals()[menuOptions[userip]]()
+    else:
+        setattr(student, menuOptions[userip], globals()[menuOptions[userip]]())
+        print(getattr(student, menuOptions[userip]))
+
+
+
+while (True):
+    # student1 = Student(None, None, None, None)
+    print('\n Options in menu\n ');
+    for option in menuOptions:
+        print(menuOptions[option] + '\n')
+    try:
+        validatedmenuoption = menu_validation(menuOptions)
+    except :
+
         print('error try again\n\n\n\n')
-      
-
-
-
-        
-
-
-
-
-
--- nerdScore_StudentID.py
+++ nerdScore_StudentID.py
@@ -1,31 +1,32 @@
-#Author: 
-#Date:
+# Author:
+# Date:
 
-#Functionality: cailculate the skill score by the equation
-#x, y, z are inputs
+# Functionality: cailculate the skill score by the equation
+# x, y, z are inputs
 def calculateSkillEquation(FandomScore, HobbiesScore, SportsNum):
-	skillScore = 0  #intialize the output list
-	
-	#Please write your own program here
-	
-        x = FandomScore, y = HobbiesScore, z = SportsNum;
-                try: 
-                        skillScore = ((((y*y)*42)/(z+1)))^(1/x)
-                except:
-                        print('error calculating result')
-                        skillScore = 0
-                        
-                return skillScore
+    skillScore = 0  # intialize the output list
 
-                
-	
+    # Please write your own program here
+
+    x = FandomScore
+    y = HobbiesScore
+    z = SportsNum;
+    try:
+        skillScore = (((((y * y) * 42) / (z + 1))) ** (1 / 2))*x
+    except:
+        print('error calculating result')
+        skillScore = 0
+
+    return skillScore
+
+
 if __name__ == '__main__':
 
-	FandomScore, HobbiesScore, SportsNum = 1, 4, 1 #the output should be 18.33030277982336
+    FandomScore, HobbiesScore, SportsNum = 1, 4, 1  # the output should be 18.33030277982336
 
-	try:
-		print(calculateSkillEquation(FandomScore, HobbiesScore, SportsNum))
-	
-	except e:
-		print(e)
-		raise	
+    try:
+        print(calculateSkillEquation(FandomScore, HobbiesScore, SportsNum))
+
+    except e:
+        print(e)
+        raise
